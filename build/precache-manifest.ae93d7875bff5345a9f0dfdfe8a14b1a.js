5ffa42424711013020f1f59c99e721af38e4eb64
Mario <mario.nascimento@aluno.uece.br>
2019-10-17 05:41:31 -0300

    re

---
+++ build/precache-manifest.ae93d7875bff5345a9f0dfdfe8a14b1a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0265ca8470d7b2b348616e2a0419a061",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf128a2db53f6baea222",
+    "url": "/static/css/main.4a428406.chunk.css"
+  },
+  {
+    "revision": "3d1b45d0b617343745d5",
+    "url": "/static/js/2.5a353542.chunk.js"
+  },
+  {
+    "revision": "bf128a2db53f6baea222",
+    "url": "/static/js/main.c96aab85.chunk.js"
+  },
+  {
+    "revision": "a659c513b913be4e7b37",
+    "url": "/static/js/runtime-main.d52357dd.js"
+  },
+  {
+    "revision": "8b67f45d4edd8023ad1c5f7c5a4ceb2b",
+    "url": "/static/media/erro-img.8b67f45d.png"
+  },
+  {
+    "revision": "10054149be4e56ead8a1e146d4f65870",
+    "url": "/static/media/logo-titulo.10054149.png"
+  },
+  {
+    "revision": "21a33e2524c129ccd6d40ac0934724ce",
+    "url": "/static/media/logo_grande.21a33e25.png"
+  }
+]);
